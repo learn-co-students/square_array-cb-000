010933a511f2ebe33fc97d4e67514133fe05d870
maxgrok <maxgrok2011@gmail.com>
2016-12-18 16:50:54 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  index = 0
+  new_array = []
+  length = array.size
+  array.each do
+	  	if length > index
+	  		square = array[index] * array[index]
+	  		new_array.push(square)
+	  		index += 1
+	  	end
+  	end
+  	return new_array
 end
+
+square_array([1,2,3])
