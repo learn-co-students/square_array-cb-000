0b81f09577176b80fd1831e8de71673e996a62c4
John Hillegass <cyrodil@Dragonborn.local>
2017-02-18 14:44:34 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
+
 def square_array(array)
-  # your code here
+  # ! doesn't mutate the array soooo will store values in a new one
+  arr = []
+  array.each do |num|
+    arr.push(num * num)
+  end
+  arr
 end
+
+
+# ! def square_array(array)
+# ! array.collect { |e| e**2 }
+# ! end
+
+
