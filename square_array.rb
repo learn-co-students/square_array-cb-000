ba44f6806490276aee4a9963274502e1cccbdc5b
Tony Nassif <tonynassif@gmail.com>
2017-05-27 21:25:33 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each { |element| squared_array.push(element ** 2) }
+  return squared_array
 end
+
+
+=begin alternate solution
+def square_array(array)
+  array.collect! { |element| element ** 2}
+end
+=end
