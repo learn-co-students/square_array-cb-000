ba4d8ebd8097ea95e9fc3aa26c281e20be068dab
Mohammad Khan <sparrowangels@gmail.com>
2017-09-02 06:13:34 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  array_squared = []
+    array.each { |value| array_squared << value * value}
+  array_squared
 end
+
+# You will build a method, square_array, that squares each element in an array of numbers and
+#  returns a new array of these squared numbers. Use an iterator and implement your own logic,
+#   don't use any built-in array methods other than .each (e.g. .collect, .inject).
+# A couple of questions that you can ask yourself are:
+# What is the return value of calling .each on an array?
+# How can I operate on each element of an array and also collect or store those elements?
+# How can I square a number? Hint: There is more than one way to do it! Google Ruby's exponent operator or refer back to the earlier lesson on simple math.
