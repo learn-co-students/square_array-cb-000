5db8a34d2b43aa817e5a5ce260ef892a56895627
Ni Chia <nichia@gmail.com>
2017-10-10 12:41:02 +0000

    square an array

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
   # your code here
+  squared_array = []
+  array.each { |num| squared_array << num * num }
+  return squared_array
+
+  # array.collect { |num| num ** 2}
 end
