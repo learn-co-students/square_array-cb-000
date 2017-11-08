7273dabc32a3f65d7bf92725be76a9c8846ff6d6
Heather Christman <efl7a@yahoo.de>
2017-11-08 19:24:29 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  index =0
+  array.each do |array_element|
+    squared_array[index] = array_element**2
+    index += 1
+  end
+  squared_array
 end
