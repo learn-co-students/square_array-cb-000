02fd63c0b802e2062f7c5c1e87ffef23512fa634
Felice Forby <feliceforby@gmail.com>
2018-04-12 22:57:55 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,15 @@
+# def square_array(array)
+#   squared_numbers = []
+#   array.each do |number|
+#     squared_numbers << number**2
+#   end
+#   return squared_numbers
+# end
+
 def square_array(array)
   squared_numbers = []
-  array.each do |number|
-    squared_numbers << number**2
-  end
-  return squared_numbers
+  array.each { |number| squared_numbers << number**2 }
+  squared_numbers
 end
 
 def square_array_with_map(array)
