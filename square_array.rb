0fa6f555ac063d6c12cc5ec91dadc0dac15b3b46
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-05-17 10:35:56 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,5 @@
-# def square_array(array)
-#   x = []
-#   array.each { |i| x << i ** 2 }
-#   x
-# end
-
-
 def square_array(array)
-  array.each { |i| i = i ** 2 }
+  x = []
+  array.each { |i| x << i ** 2 }
+  x
 end
