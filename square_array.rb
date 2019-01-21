b8c39315e92e830b08cd6f48e8b7dacd85154bf5
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-01-21 04:44:05 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
+def square_array(numbers)
   # your code here
+  square_array = Array.new
+  #numbers = [1, 2, 3]
+  
+  numbers.each do |squares|
+    b = squares**2
+    square_array << b
+  end
+  square_array
 end
